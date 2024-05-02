297b9ecf12a13cbe47d604d567d7c9f0b09c2067
Enver <envermilligan@gmail.com>
2024-05-02 20:20:17 +0300

    add heder component

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -1,7 +1,19 @@
+import React, { useState } from 'react';
+import Header from '../Header/Header';
 import s from './App.module.css';
 
 function App() {
-  return <div className={s.app}>Hello World</div>;
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
+
+  return (
+    <div className={darkTheme ? s.app_dark : s.app}>
+      <Header darkTheme={darkTheme} toggleTheme={toggleTheme} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Component/Header/Header.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Github from '../../Lib/Icon/contacts/github.svg';
+import Twitter from '../../Lib/Icon/contacts/twitter.svg';
+import Logo from '../../Lib/Icon/logo.svg';
+import Moon from '../../Lib/Icon/contacts/moon.svg';
+import s from './Header.module.css';
+
+export default function Header({ darkTheme, toggleTheme }) {
+  const items = [
+    { id: 1, name: 'Home' },
+    { id: 2, name: 'About' },
+    { id: 3, name: 'Tech Stack' },
+    { id: 4, name: 'Projects' },
+    { id: 5, name: 'Contact' },
+  ];
+
+  return (
+    <div className={darkTheme ? `${s.header} ${s.dark}` : s.header}>
+      <img src={Logo} alt='logo' />
+      <div className={s.heder_btns}>
+        {items.map((item) => (
+          <button className={s.btn} key={item.id}>
+            {item.name}
+          </button>
+        ))}
+      </div>
+      <div className={s.contscts}>
+        <div className={s.hover}>
+          <a href='#github'>
+            <img src={Github} alt='github' />
+          </a>
+        </div>
+        <div className={s.hover}>
+          <a href='#twitter'>
+            <img src={Twitter} alt='twitter' />
+          </a>
+        </div>
+        <div className={s.hover}>
+          <button onClick={toggleTheme} className={s.theme}>
+            <img src={Moon} alt='moon' />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
